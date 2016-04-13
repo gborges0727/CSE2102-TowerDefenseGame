877768d608a0c6ea402e173aba7a935f0354e531
Gabriel Borges <gborges0727@gmail.com>
2016-04-12 23:34:20 -0400

    Add base constructor to Tower

--- SWTowerDefense/SWTowerDefense/Tower.swift
+++ SWTowerDefense/SWTowerDefense/Tower.swift
@@ -18,6 +18,16 @@ class Tower: SKSpriteNode {
     var damage = 10
     var fireRate = 1
     var towerCost = 100
+    var currentEnemy: SKSpriteNode!
+    
+    init() {
+        let texture = SKTexture(imageNamed: "blueSquare")
+        super.init(texture: texture, color: SKColor.clearColor(), size: texture.size())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     func selectEnemy() {
         // Function used to get a target enemy
