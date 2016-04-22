0609b0ef595886658e4bd84182c332cc305d40f8
Gabriel Borges <gborges0727@gmail.com>
2016-04-22 15:14:33 -0400

    Update GameViewController to start with menuscene

--- SWTowerDefense/SWTowerDefense/GameViewController.swift
+++ SWTowerDefense/SWTowerDefense/GameViewController.swift
@@ -16,7 +16,7 @@ extension SKNode {
             let archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
             
             archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! GameScene
+            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! MenuScene
             archiver.finishDecoding()
             return scene
         } else {
@@ -30,20 +30,13 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .ResizeFill
-            
-            skView.presentScene(scene)
-        }
+        let scene = MenuScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .ResizeFill
+        skView.presentScene(scene)
     }
 
     override func shouldAutorotate() -> Bool {
